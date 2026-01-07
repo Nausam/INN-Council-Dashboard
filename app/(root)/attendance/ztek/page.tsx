c21baaccc6218301a29b912a8ee9797f1fb94486
CodeEternalCode <codeeternal@gmail.com>
2026-01-07 09:04:54 +0500

    implemented waste

--- app/(root)/attendance/ztek/page.tsx
+++
@@ -1,9 +0,0 @@
-import AttendanceSyncDashboard from "@/components/AttendanceSyncDashboard";
-
-export default function AttendancePage() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <AttendanceSyncDashboard />
-    </div>
-  );
-}
