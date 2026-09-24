dfd27adbf84542aa2851f0076c64b1674d9e5ac1
CodeEternalCode <codeeternal@gmail.com>
2026-09-24 13:53:14 +0500

    Allow XML control character sanitation in leave form

--- lib/forms/salaam-family-leave.ts
+++ lib/forms/salaam-family-leave.ts
@@ -35,6 +35,8 @@ const SLOT = {
 
 function escapeXml(value: string): string {
   return value
+    // XML forbids these control characters; removing them is intentional.
+    // eslint-disable-next-line no-control-regex
     .replace(/[\u0000-\u0008\u000b\u000c\u000e-\u001f]/g, "")
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
